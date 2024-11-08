308f5ef06d603bde06d5f204efde9b0fb04801c2
Rohan <rohan26ir@gmail.com>
2024-11-08 23:51:13 +0600

    Update all

--- src/Components/Utility/addToWish.js
+++ src/Components/Utility/addToWish.js
@@ -1,18 +1,18 @@
 import { toast } from 'react-toastify';
 
 const getStoredWish = () => {
-  const storedListStr = localStorage.getItem('read-list');
+  const storedListStr = localStorage.getItem('wish-list');
   return storedListStr ? JSON.parse(storedListStr) : [];
 };
 
 const addToStoredWish = (id) => {
   const storedList = getStoredWish();
   if (storedList.includes(id)) {
-    console.log(id, "already exists in the read list");
-    toast.error("already exists in the Wishlist", { position: "top-center" });
+    console.log(id, "already exists in the wish list");
+    toast.error("Already exists in the Wishlist", { position: "top-center" });
   } else {
     storedList.push(id);
-    localStorage.setItem('read-list', JSON.stringify(storedList));
+    localStorage.setItem('wish-list', JSON.stringify(storedList));
     toast.success("This Device is added to your WishList", { position: "top-center" });
   }
 };
@@ -20,7 +20,7 @@ const addToStoredWish = (id) => {
 const removeFromStoredWish = (id) => {
   const storedList = getStoredWish();
   const updatedList = storedList.filter(itemId => itemId !== id);
-  localStorage.setItem('read-list', JSON.stringify(updatedList));
+  localStorage.setItem('wish-list', JSON.stringify(updatedList));
   toast("Remove the Device from Wishlist")
 };
 
